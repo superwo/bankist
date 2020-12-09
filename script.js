d2905a3aab804687d83ccca49faa16b05413c57e
Avadanii Radu <radu.avadanii@bludelego.it>
2020-12-09 11:22:42 +0200

    find method

--- script.js
+++ script.js
@@ -81,7 +81,7 @@ const displayMovements = function (movements) {
   });
 }
 
-displayMovements(account1.movements);
+
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, cur) => acc + cur, 0);
@@ -89,15 +89,15 @@ const calcDisplayBalance = function (movements) {
   labelBalance.textContent = `${balance} €`;
 }
 
-calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements.filter(mov => mov > 0).reduce((acc, cur) => acc + cur, 0);
 
-  const out = movements.filter(mov => mov < 0).reduce((acc, cur) => acc + cur, 0);
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements.filter(mov => mov > 0).reduce((acc, cur) => acc + cur, 0);
 
-  const interest = movements.filter(mov => mov > 0)
-    .map(deposit => (deposit * 1.2) / 100)
+  const out = acc.movements.filter(mov => mov < 0).reduce((acc, cur) => acc + cur, 0);
+
+  const interest = acc.movements.filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
     .filter(dep => dep > 1)
     .reduce((acc, dep) => acc + dep, 0);
 
@@ -106,7 +106,7 @@ const calcDisplaySummary = function (movements) {
   labelSumInterest.textContent = `${interest} €`;
 }
 
-calcDisplaySummary(account1.movements);
+
 
 const createUsernames = function (accs) {
   accs.forEach(acc => {
@@ -119,6 +119,32 @@ const createUsernames = function (accs) {
 }
 
 createUsernames(accounts);
+
+// Event handling
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+
+  console.log(currentAccount)
+
+  if (currentAccount && currentAccount.pin === Number(inputLoginPin.value)) {
+    labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
+
+    containerApp.style.opacity = 100;
+
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    displayMovements(currentAccount.movements);
+
+    calcDisplayBalance(currentAccount.movements);
+
+    calcDisplaySummary(currentAccount);
+  }
+})
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
